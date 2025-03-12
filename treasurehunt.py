ed8c3fff75264fd62d70040efb40241660ee9c8a
POKALA SAI NITHIN <151384520+Nithyon@users.noreply.github.com>
2025-03-12 18:05:51 +0530

    Update and rename solution.py to treasurehunt.py

--- treasurehunt.py
+++ treasurehunt.py
@@ -22,29 +22,27 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-choice1 = input('You\'re at a crossroad, where do you want to go? '
-                'Type "left" or "right".\n').lower()
+choice1 = input('You\'re at the crossroads, where do you want to go? Type "left" or "right".  ').lower()
 
-if choice1 == "left":
-    choice2 = input('You\'ve come to a lake. '
-                    'There is an island in the middle of the lake. '
-                    'Type "wait" to wait for a boat. '
-                    'Type "swim" to swim across.\n').lower()
-    if choice2 == "wait":
-        choice3 = input("You arrive at the island unharmed. "
-                        "There is house with 3 doors. One red, "
-                        "one yellow and one blue. "
-                        "Which colour do you choose?\n").lower()
-        if choice3 == "red":
-            print("It's a room full of fire. Game Over")
-        elif choice3 == "yellow":
-            print("You found the treasure. You Win!")
-        elif choice3 == "blue":
-            print("You enter a room of beasts. Game Over.")
-        else:
-            print("You chose a door that doesn't exist. Game Over.")
-    else:
-        print("You got attacked by an angry trout. Game Over.")
+if choice1 == "left" :
 
+    if choice1 == "right":
+        print("Fell into into a hole. Game Over.")
 else:
-    print("You fell in to a hole. Game Over.")
+    print("Game Over.")
+
+choice2 = input(' you\'re currently in the swimming pool, what you wanna do ? Type "Swim" or "wait". ').lower()
+if choice2 == "wait":
+    if choice2 == "swim":
+        print("there is a trout spotted , you were attacked by the trout . Game over.")
+else :
+    print("Game over")
+
+choice3 = input('your in front of a door , you have three options choose one "red" or "yellow" or "blue" ').lower()
+print("you entered a fire realm, you got burnt by fire")
+if choice3 == "yellow":
+    print("You win the eternal treasure")
+if choice3 == "blue":
+    print("You entered into a beast realm , you got eaten by the beasts, Game Over.")
+else:
+    print("Game over")
